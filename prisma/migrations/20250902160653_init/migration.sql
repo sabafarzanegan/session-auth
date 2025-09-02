ab0c0b47b549e2009ae9264a2a2e98a0d958a2e4
saba farzanegan <saba.far.99@gmail.com>
2025-09-02 19:42:31 +0330

    add prisma,redis,shadcn setup

---
+++ prisma/migrations/20250902160653_init/migration.sql
@@ -0,0 +1,16 @@
+-- CreateEnum
+CREATE TYPE "public"."Role" AS ENUM ('Admin', 'User');
+
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "salt" TEXT NOT NULL,
+    "role" "public"."Role" NOT NULL DEFAULT 'User',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
